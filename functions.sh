05f88793478a11ccb2bbb3501723bfe85865f7f5
Frieren360 <162036597+Frieren360@users.noreply.github.com>
2026-05-12 12:58:47 +0100

    Fix syntax error in ZFS_USED assignment

--- functions.sh
+++ functions.sh
@@ -50,6 +50,6 @@ resize_lxc() {
         [ -z "$lxc_size" ] && error_message missing_size_lxc
         
         PVE_NAME=$(zfs list | grep "$arg" | awk '{print $1}')
-        ZFS_USED="$(zfs list $PVE_NAME | awk 'NR > 1 {print $2}'"
+        ZFS_USED="$(zfs list $PVE_NAME | awk 'NR > 1 {print $2}')"
         printf "%d\n" "$(to_bytes $ZFS_USED)"
 }
